06a96ce8e12b3da9901613b063c79fb6c596708b
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-03-16 17:01:42 +0530

    Update CO23AD0601_4_A1.py

--- CO23AD0601_4_A1.py
+++ CO23AD0601_4_A1.py
@@ -1,8 +1,8 @@
 ballACoord = 10.9
 ballBCoord = 100.8
-ballA_mass = 2
+ballAMass = 2
 ballBMass = 3
 distance = ballBCoord - ballACoord
 valueOfG = 0.00000000006674
-__force = (valueOfG * ballA_mass * ballBMass) / (distance * distance)
+__force = (valueOfG * ballAMass * ballBMass) / (distance * distance)
 print(__force)
